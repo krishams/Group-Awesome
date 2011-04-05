c5a85cdb2637bd82ef1a5282583f16d5a7595d8a
William Holm Jacobsen <whjacobsen@gmail.com>
2011-04-05 12:29:42 +0200

    har opdateret autoload, og index.php

--- application/views/index_view.php
+++ application/views/index_view.php
@@ -8,13 +8,25 @@ and open the template in the editor.
 
     <table>
         <tr>
-            <td>Email</td><td><input type="text" name="emailfield" size="40"/></td>
+            <td>Email</td><td><input type="text" name="emailfield" size="30"/></td>
         </tr>
         <tr>
-            <td>Password</td><td><input type="password" name="passwfield" size="40" /></td>
+            <td>Password</td><td><input type="password" name="passwfield" size="30" /></td>
         </tr>
-        <input type="submit" value="Sign in"/>
+        <tr>
+            <td>&nbsp;</td>
+            <td><input type="submit" value="Sign in"/><td>
+        </tr>
+        <tr>
+            <td>&nbsp;</td>
+            <td><?php echo anchor('main_controller/getRegistration', 'Not signed up?'); ?></td>
+        </tr>
+        <tr>
+            <td>&nbsp;</td>
+            <td><?php echo anchor('main_controller/getRequestPassword', 'Forgot your password?'); ?></td>
+        </tr>
         
     </table>
 
 </div>
+
