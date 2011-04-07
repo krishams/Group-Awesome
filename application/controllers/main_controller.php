6d387a03863c900fa411afb695eaedddb2002afe
Oliver Kinzl <oliverkinzl@hotmail.com>
2011-04-07 10:09:02 +0200

    fix conflict

--- application/controllers/main_controller.php
+++ application/controllers/main_controller.php
@@ -42,13 +42,37 @@ class Main_Controller extends CI_Controller {
     }
 
     /**
-     * Saves the user registration information and sends a confirmation email with a link to log on
-     * @param <array> $data the user's registration information
+     * Saves the user registration information and sends a confirmation email with a link to log on.
+     * First we load the helper form_validation, which makes it possible to set som rules for our form.
+     * in the rules we 3 parameters, 1: the fieldname, 2: errormessage and 3: the validation rule.
+     * trim means removing malicious code.
      */
-    function submitRegistration($data)
-    {
-        $data['main_content'] = 'checkMail_view';
-        $this->load->view('/include/template_view', $data);
+    function submitRegistration() {
+        //check if the captha is correct
+        //control that all fields have data in them, if not display which is missing - done
+        //check if email already exist in db, if not then display error - ½ done
+
+        $this->load->library('form_validation');
+
+        $this->form_validation->set_rules('firstname', 'First Name', 'trim|required');
+        $this->form_validation->set_rules('lastname', 'Last Name', 'trim|required');
+        $this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email');
+        $this->form_validation->set_rules('passw', 'Password', 'trim|required|min_length[4]|max_length[32]');
+        $this->form_validation->set_rules('confirmPassw', 'Confirm Password', 'trim|required|matches[passw]');
+
+        if ($this->form_validation->run() == FALSE) {
+            $this->getRegistration();
+        } else if ($this->main_model->emailValidation()) {
+            redirect($uri = 'registration_view');
+            //print error message
+        } else {
+            if ($this->main_model->saveUserdata()) {
+                $data['main_content'] = 'checkMail_view';
+                $this->load->view('include/template_view', $data);
+            } else {
+                $this->load->view('registration_view');
+            }
+        }
     }
 
     /**
@@ -90,70 +114,6 @@ class Main_Controller extends CI_Controller {
 //        else
 //            redirect();
 
-    /**
-     *
-     */
-    function getRegistration() {
-        $data['main_content'] = 'registration_view';
-        $this->load->view('/include/template_view', $data);
-    }
-
-    /**
-     * Saves the user registration information and sends a confirmation email with a link to log on.
-     * First we load the helper form_validation, which makes it possible to set som rules for our form.
-     * in the rules we 3 parameters, 1: the fieldname, 2: errormessage and 3: the validation rule.
-     * trim means removing malicious code.
-     */
-    function submitRegistration() {
-        //check if the captha is correct
-        //control that all fields have data in them, if not display which is missing - done
-        //check if email already exist in db, if not then display error - ½ done
-
-        $this->load->library('form_validation');
-
-        $this->form_validation->set_rules('firstname', 'First Name', 'trim|required');
-        $this->form_validation->set_rules('lastname', 'Last Name', 'trim|required');
-        $this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email');
-        $this->form_validation->set_rules('passw', 'Password', 'trim|required|min_length[4]|max_length[32]');
-        $this->form_validation->set_rules('confirmPassw', 'Confirm Password', 'trim|required|matches[passw]');
-
-        if ($this->form_validation->run() == FALSE) {
-            $this->getRegistration();
-        } else if ($this->main_model->emailValidation()) {
-            redirect($uri = 'registration_view');
-            //print error message
-        } else {
-            if ($this->main_model->saveUserdata()) {
-                $data['main_content'] = 'checkMail_view';
-                $this->load->view('include/template_view', $data);
-            } else {
-                $this->load->view('registration_view');
-            }
-        }
-    }
-
-    function getRequestPassword() {
-        $data['main_content'] = 'requestPass_view';
-        $this->load->view('/include/template_view', $data);
-    }
-
-    /**
-     * Checks if the email exists and if so, sends a reset link.
-     * If not, the user is alerted so they can re-type the password.
-     * @param <string> $email
-     */
-    function submitRequestPassword($email) {
-
-    }
-
-    /**
-     * Checks the database to see if the email exists
-     * @param <string> $email the users email address
-     * @return <boolean> whether the email exists
-     */
-    function emailExistsInDb() {
-        return true;
-    }
 
     /**
      *
