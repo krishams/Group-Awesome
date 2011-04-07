0db96a77a2af1f930f8d232df9a5bc24deec1bc8
William Holm Jacobsen <whjacobsen@gmail.com>
2011-04-07 18:10:21 +0200

    aendringer i resetpass_view og slettet gamle user_controller

---
+++ application/views/resetPass_view.php
@@ -0,0 +1,23 @@
+<div class="resetPassDiv">
+    <?php echo form_open(main_controller/resetPassSuccess_view); ?> 
+        <table class="resetTable">
+            <tr>
+                <td>Password:</td>
+                <td><?php echo form_password('passw','');?></td>
+            </tr>
+            <tr>
+                <td>Confirm password:</td>
+                <td><?php echo form_password('confirmPassw');?></td>
+            </tr>
+            <tr>
+            <td>&nbsp;</td>
+            <td><input type="submit" value="Submit" /><td>
+        	</tr>
+        </table>
+        <br />
+        <?php echo anchor('', 'Back to sign in') ?>
+        <!-- Is used to display type in errors-->
+        <?php echo validation_errors('<p class="error">'); ?>
+
+    <?php echo form_close(); ?>
+</div>
