390cd1dd5454a801d9489b6f620d7e472ed5c709
William Holm Jacobsen <whjacobsen@gmail.com>
2011-04-07 10:32:48 +0200

    tilfojet checkmail2

---
+++ application/views/checkMail2_view.php
@@ -0,0 +1,10 @@
+<div id="checkMail">
+
+    <p>Success <br/>
+       A confirmation email has now been sent to your email inbox, <br/>
+       please go to your email and reset your password by clicking the activation link before you sign in.
+    </p>
+
+    <?php echo anchor('', 'Back to sign in') ?>
+
+</div>
