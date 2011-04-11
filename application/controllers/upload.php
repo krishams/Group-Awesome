4be4ef07ab73634e0370de1f6c09a17d8a1cc039
William Holm Jacobsen <whjacobsen@gmail.com>
2011-04-11 14:10:01 +0200

    tilfojet nogle filer til upload image

---
+++ application/controllers/upload.php
@@ -0,0 +1,46 @@
+<?php
+
+class Upload extends CI_Controller {
+
+    function  __construct() {
+        parent::__construct();
+        session_start();
+    }
+
+      function getUpload() {
+        $data['main_content'] = 'upload_view';
+        $this->load->view('/include/template1_view', $data);
+    }
+
+
+    /**
+     * This function is called when a user is uploading a profile picture
+     */
+    function do_upload()
+	{
+		$config['upload_path'] = './assets/img/profilePics/';
+		$config['allowed_types'] = 'gif|jpg|png';
+		$config['max_size']	= '0';
+		$config['max_width']  = '0';
+		$config['max_height']  = '0';
+
+		$this->load->library('upload', $config);
+
+		if ( ! $this->upload->do_upload())
+		{
+			$error = array('error' => $this->upload->display_errors());
+
+			$this->load->view('upload_view', $error);
+
+		}
+		else
+		{
+			$data = array('upload_data' => $this->upload->data());
+
+			$this->load->view('uploadSuccess_view', $data);
+		}
+	}
+
+}
+
+?>
