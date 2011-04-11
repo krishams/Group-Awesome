d3eb2ba2fe44e83d319a0f2a3684703dff610c25
Kristian Hjaltason <kwhjaltason@hotmail.com>
2011-04-11 13:43:02 +0200

    Added user_model

---
+++ application/models/user_model.php
@@ -0,0 +1,17 @@
+<?php
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Kristian
+ */
+class user_model extends CI_Model {
+
+    function user_model() {
+        parent::__construct();
+    }
+}
+?>
