1b46542cba3113c636c9660df40dc9bf28cba915
Kristoffer Hammer <kristoffer@hammernet.org>
2011-04-11 14:06:06 +0200

    Added form validation to editprofile
    Signed-off-by: Kristoffer Hammer <kristoffer@hammernet.org>

---
+++ application/views/showEditProfile_view.php
@@ -0,0 +1,29 @@
+<h2>Edit profile view</h2>
+
+<form name='input' action='<?php echo base_url()?>user/editProfile' method='post'>
+	<table>
+		<tr>
+			<td>Firstname:</td><td><input type='text' name='firstname' value='<?php echo $profile['f_name']?>'/></td>
+		</tr>
+		<tr>
+			<td>Lastname:</td><td><input type='text' name='lastname' value='<?php echo $profile['l_name']?>'/></td>
+		</tr>
+		<tr>
+			<td>E-Mail:</td><td><input type='text' name='email' value='<?php echo $profile['email']?>'/></td>
+		</tr>
+		<tr>
+			<td>Old password:</td><td><input type='text' name='Oldpsw'/></td>
+		</tr>
+		<tr>
+			<td>New password:</td><td><input type='text' name='passw'/></td>
+		</tr>
+		<tr>
+			<td>New password again:</td><td><input type='text' name='confirmPassw'/></td>
+		</tr>
+		<tr>
+			<td><input type='Submit' value='Save Changes'</td>
+		</tr>				
+	</table>
+	<?php echo validation_errors('<p class="error">'); ?>
+</form>
+
