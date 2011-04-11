861d1daf258f0c33334164a0ef5281712472abc9
Kristian Hjaltason <kwhjaltason@hotmail.com>
2011-04-11 10:21:15 +0200

    Moved the showProfile to user_controller from main

--- application/controllers/user_controller.php
+++ application/controllers/user_controller.php
@@ -14,5 +14,22 @@ class User_Controller extends CI_Controller {
     function __construct() {
         parent::__construct();
     }
+
+    /**
+     * Will pass the users id to the db, and gets all the users data back, and
+     * forwards it to the users profilpage
+     */
+    function showProfile() {
+
+        $userid = 0;
+
+        if ($this->uri->segment(3)) {
+            $userid = $this->uri->segment(3);
+        }
+        $data['profile'] = $this->main_model->getUserById($userid);
+
+        $data['main_content'] = 'profile_view';
+        $this->load->view('include/template1_view', $data);
+    }
 }
 ?>
