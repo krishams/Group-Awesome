e84220e72f0ce3ed751214e2569bd6d6976f47f9
Kristoffer Hammer <kristoffer@hammernet.org>
2011-04-15 12:04:47 +0200

    smal change in navigation
    Signed-off-by: Kristoffer Hammer <kristoffer@hammernet.org>

--- application/views/include/navigation_view.php
+++ application/views/include/navigation_view.php
@@ -1,5 +1,5 @@
 <ul class="headerUl">
-    <li class="<?=($this->uri->segment(2)==='home')?'active':''?>"> ><a class="navigationBar" href="<?php echo base_url()?>main/index" >Home</a></li>
+    <li class="<?=($this->uri->segment(2)==='index')?'active':''?>"> ><a class="navigationBar" href="<?php echo base_url()?>main/index" >Home</a></li>
     <li class="<?=($this->uri->segment(2)==='showProfile')?'active':''?>">><a class="navigationBar" href="<?php echo base_url()?>user/showProfile/<?php echo $_SESSION['userid']; ?>">Profile</a></li>
     <li class="<?=($this->uri->segment(2)==='showEditProfile')?'active':''?>">><a class="navigationBar" href="<?php echo base_url()?>user/showEditProfile/<?php echo $_SESSION['userid']; ?>">Account</a></li>
     <li class="<?=($this->uri->segment(2)==='logOut')?'active':''?>">><a class="navigationBar" href="<?php echo base_url()?>main/logOut" >Log out</a></li>
