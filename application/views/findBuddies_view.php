daef0c042d6e801a3d61d2678fdb9bc6806c1623
William Holm Jacobsen <whjacobsen@gmail.com>
2011-04-26 14:32:23 +0200

    rykket rundt paa search user tekst feltet

--- application/views/findBuddies_view.php
+++ application/views/findBuddies_view.php
@@ -1,3 +1,11 @@
+<br/><br/>
+<?php
+
+        $attributes = array('method' => 'post'); echo form_open('main/searchUserButton/',$attributes);
+        echo '<li><input type="text" name="search" onkeyup="showResult(this.value)"/> </li>
+    	<li><input type="submit" value="Search user" /> </li>';
+    	echo form_close();
+?>
 <div id="findBuddies">
    <h1>Find Buddies</h1>
    <br/>
@@ -7,11 +15,6 @@
         {
            echo '<a href="'.base_url().'user/showProfile/'.$row['user_id'].'"><img width="155px" height="180px" src="'.$row['path'].'"></a>';
         }
-        
-		$attributes = array('method' => 'post'); echo form_open('main/searchUserButton/',$attributes);
-		echo '<li><input type="text" name="search" onkeyup="showResult(this.value)"/> </li>
-    	<li><input type="submit" value="Search user" /> </li>';
-    	echo form_close();
    ?>
 
 </div>
