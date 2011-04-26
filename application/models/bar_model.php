a2ceebbc2aa88b49d8f778ef43edb9c1f375753f
Oliver Kinzl <oliverkinzl@hotmail.com>
2011-04-26 12:03:25 +0200

    bar methods modififed

--- application/models/bar_model.php
+++ application/models/bar_model.php
@@ -12,6 +12,10 @@ class bar_model extends CI_Model {
         parent::__construct();
     }
 
+    /**
+     * Gets a list of bars in the database
+     * @return Array of bars. Each array element has [id] and [name]
+     */
     function getListOfBars() {
         $this->db->order_by('name');
         $Q = $this->db->get('bars');
@@ -25,20 +29,16 @@ class bar_model extends CI_Model {
         return $data;
     }
 
-    function getFavoriteBars() {
-        $this->db->select('bar_id')->from('bars')->where('id', $id)->limit(10, 20);
-
-/* NEED VIEW
- *  SELECT favorite_bars.user_id, favorite_bars.bar_id, bars.name
-FROM `favorite_bars`, INNER JOIN 'bars'
-ON favorite_bars.bar_id = bars.id
- *
- */
-        $query = $this->db->get();
+    /**
+     * Gets a user's favorite bars
+     * @param int $id the user's id #
+     * @return Array a list of the favorite bars. Each array element has [bar_id] and [name]
+     */
+    function getFavoriteBars($id) {
+        $this->db->select('bar_id, name')->from('bar_view')->where('user_id', $id);
+        $Q = $this->db->get();
 
         $data = array();
-        $this->db->order_by('name');
-        $Q = $this->db->get('Users');
         if ($Q->num_rows() > 0) {
             foreach ($Q->result_array() as $row) {
                 $data[] = $row;
@@ -63,11 +63,11 @@ ON favorite_bars.bar_id = bars.id
         return $data;
     }
 
-    function saveBar() {
+    function createBar() {
 
     }
 
-    function saveFavoriteBar() {
+    function saveFavoriteBar($user_id, $bar_id) {
 
     }
 
