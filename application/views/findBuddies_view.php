1d892f5a842b423059351acb02f6d728e8d90406
Oliver Kinzl <oliverkinzl@hotmail.com>
2011-04-27 13:34:31 +0200

    modifying findBuddies_view

--- application/views/findBuddies_view.php
+++ application/views/findBuddies_view.php
@@ -1,14 +1,26 @@
-<br/><br/>
+<h1>Find Buddies</h1>
 <?php
+        echo '<div id="findBuddies"><h2>Search by Name:</h2>';
 
-        $attributes = array('method' => 'post'); echo form_open('main/searchUserButton/',$attributes);
+        $attributes = array('method' => 'post');
+        echo form_open('main/searchUserButton/',$attributes);
         echo '<li><input type="text" name="search" onkeyup="showResult(this.value)"/> </li>
     	<li><input type="submit" value="Search user" /> </li>';
     	echo form_close();
+        echo '</div>';
+
+        $bar_options = '<div><h2>Find buddies at your favorite bars!</h2><select id = "search_bar_users">';
+        $bar_options .= '<option selected="selected">--</option>';
+        foreach($favorite_bars as $bar) {
+            $bar_options .= '<option id = "' . $bar['bar_id'] . '">' . $bar['name'] . '</option>';
+        }
+        $bar_options .= '</select></div>';
+
+        echo $bar_options;
 ?>
-<div id="findBuddies">
-   <h1>Find Buddies</h1>
-   <br/>
+<div id = "search_results">results:</div>
+
+<div id = "randomUsers"><h1>Random Users</h1>
 
    <?php
         foreach($random as $row)
@@ -18,3 +30,4 @@
    ?>
 
 </div>
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/search.js"></script>
--- application/views/include/footer_view.php
+++ application/views/include/footer_view.php
@@ -2,7 +2,7 @@
 
 </div> <!-- end wrapper -->
 <ul>  
-    <li class="<?= ($this->uri->segment(2) === 'login') ? 'active' : '' ?>">><a class="seemore" href="<?php echo base_url() ?>admin/login">Admin login</a></li>
+    <li class="<?= ($this->uri->segment(2) === 'login') ? 'active' : '' ?>"><a class="seemore" href="<?php echo base_url() ?>admin/login">Admin login</a></li>
 </ul>
 </body>
 
