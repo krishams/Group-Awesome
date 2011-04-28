217b50b22a6980183d81930a53b447877bf39b22
Oliver Kinzl <oliverkinzl@hotmail.com>
2011-04-28 12:36:56 +0200

    small fixes

--- application/config/config.php
+++ application/config/config.php
@@ -69,7 +69,7 @@ $config['url_suffix'] = '';
 | than english.
 |
 */
-$config['language']	= 'danish';
+$config['language']	= 'english';
 
 /*
 |--------------------------------------------------------------------------
